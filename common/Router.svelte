52a131bdde56d725083e3a072dc430af348bd9b6
NoCrypt <57245077+NoCrypt@users.noreply.github.com>
2024-07-26 00:32:20 +0700

    feat: make the back button functional

--- common/Router.svelte
+++ common/Router.svelte
@@ -14,14 +14,42 @@
   import WatchTogether from './views/WatchTogether/WatchTogether.svelte'
   import Miniplayer from 'svelte-miniplayer'
   import Search from './views/Search.svelte'
+  import { toast } from 'svelte-sonner';
   import AiringSchedule from './views/AiringSchedule.svelte'
   import { readable } from 'simple-store-svelte'
   import { files } from './views/Player/MediaHandler.svelte' // this is sooo hacky and possibly delaying viewer on startup
+  import { rss } from './views/TorrentSearch/TorrentModal.svelte';
+  import { view } from './App.svelte';
 
   export let page = 'home'
 
   $: minwidth = $isMobile ? '200px' : '35rem'
   $: maxwidth = $isMobile ? '200px' : '60rem'
+
+  if ($isMobile) {
+    let backButtonPressTimeout;
+
+    window.Capacitor.Plugins.App.addListener('backButton', () => {
+      if (page === 'home' && $view === null && $rss === null) {
+        if (backButtonPressTimeout) {
+          clearTimeout(backButtonPressTimeout);
+          window.Capacitor.Plugins.App.exitApp();
+        } else {
+          toast.warning('Press again to exit', { duration: 1000 });
+          backButtonPressTimeout = setTimeout(() => {
+            backButtonPressTimeout = null;
+          }, 1000);
+        }
+      } else {
+        page = 'home';
+        $rss = null;
+        $view = null;
+      }
+    });
+  }
+
+
+
 </script>
 
 <div class='w-full h-full position-absolute overflow-hidden' class:sr-only={($files.length === 0)}>
